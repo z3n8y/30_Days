1493cbcca0e658fa8ff300a0ee9163e4d36eb87f
z3n8y <57050965+z3n8y@users.noreply.github.com>
2020-12-01 18:32:34 -0500

    Update Calculator.java

--- src/day_17_more_exceptions/Calculator.java
+++ src/day_17_more_exceptions/Calculator.java
@@ -3,13 +3,9 @@ package day_17_more_exceptions;
 public class Calculator {
 	
 	
-	public void Message() throws Exception{
-		throw new Exception("n and p should be non-negative");
-	}
-	
 	public int power(int n, int p) throws Exception {
 		if (p < 0 | n < 0) {
-			Message();
+			throw new Exception("n and p should be non-negative");
 		}
 		if (p <= 0) { 
 			return 1;
